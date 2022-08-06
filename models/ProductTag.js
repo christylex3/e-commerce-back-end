d08a5d75d2eb0c457d86cbe2aa256a86889f3f15
Christy Le <christylex3@gmail.com>
2022-08-06 18:27:16 -0400

    Added columns to ProductTag and Tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
 	{
 		// define columns
+		// product_id: 1,
+		// tag_id: 8,
+		product_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: "product",
+				key: "product_id",
+				unique: false
+			}
+		},
+		tag_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: "tag",
+				key: "tag_id",
+				unique: false
+			}
+		}
 	},
 	{
 		sequelize,
