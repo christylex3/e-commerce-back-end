d1720c3ea0046a62c448607884a4711e3bc3d9a9
Christy Le <christylex3@gmail.com>
2022-08-06 22:26:00 -0400

    Added GET route to grab one category and their associated products. Also created a POST route to create a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,8 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
+// GET - Finds all categories and all of their associated products
 router.get("/", async (req, res) => {
-	// find all categories
-	// be sure to include its associated Products
 	try {
 		const categoryData = await Category.findAll({
 			include: [{ model: Product }],
@@ -17,21 +16,41 @@ router.get("/", async (req, res) => {
 	}
 });
 
-router.get("/:id", (req, res) => {
-	// find one category by its `id` value
-	// be sure to include its associated Products
+
+// GET - Finds one category by its `id` value and their associated products
+router.get("/:id", async (req, res) => {
+	try {
+		const categoryData = await Category.findByPk(req.params.id, {
+			include: [{ model: Product }],
+		});
+		if (!categoryData) {
+			res.status(404).json({ message: 'No category found with that id!' });
+			return;
+		}
+		res.status(200).json(categoryData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-router.post("/", (req, res) => {
-	// create a new category
+// POST - Creates new category
+router.post("/", async (req, res) => {
+	try {
+		const categoryData = await Category.create(req.body);
+		res.status(200).json(categoryData);
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
+// PUT - Updates a category by its `id` value
 router.put("/:id", (req, res) => {
-	// update a category by its `id` value
+	
 });
 
+// DELETE = Deletes a category by its `id` value
 router.delete("/:id", (req, res) => {
-	// delete a category by its `id` value
+	
 });
 
 module.exports = router;
