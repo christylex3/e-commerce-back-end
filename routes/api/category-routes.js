5a0768bb4b3d0ac17b7305e3a30945935cd500c6
Christy Le <christylex3@gmail.com>
2022-08-06 22:32:24 -0400

    Added DELETE request for category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,17 +16,18 @@ router.get("/", async (req, res) => {
 	}
 });
 
-
 // GET - Finds one category by its `id` value and their associated products
 router.get("/:id", async (req, res) => {
 	try {
 		const categoryData = await Category.findByPk(req.params.id, {
 			include: [{ model: Product }],
 		});
+
 		if (!categoryData) {
 			res.status(404).json({ message: 'No category found with that id!' });
 			return;
 		}
+
 		res.status(200).json(categoryData);
 	} catch (err) {
 		res.status(500).json(err);
@@ -49,8 +50,23 @@ router.put("/:id", (req, res) => {
 });
 
 // DELETE = Deletes a category by its `id` value
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
+	try {
+		const categoryData = await Category.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
 	
+		if (!categoryData) {
+			res.status(404).json({ message: 'No category found with that id!' });
+			return;
+		}
+	
+		res.status(200).json(readerData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
