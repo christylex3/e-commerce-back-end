b3669a22c19a35bcffef76dc6e855ef3e8d1782a
Christy Le <christylex3@gmail.com>
2022-08-07 14:27:44 -0400

    Added GET request to grab a single tag by its 'id' and its associated product data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,8 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
+// GET request to find all tags and their associated Product data
 router.get("/", async (req, res) => {
-	// find all tags
-	// be sure to include its associated Product data
 	try {
 		const tagData = await Tag.findAll({
 			include: [{ model: Product }],
@@ -16,9 +15,22 @@ router.get("/", async (req, res) => {
 	}
 });
 
+// GET request to find a single tag by its 'id' and also includes its associated Product data
 router.get("/:id", (req, res) => {
-	// find a single tag by its `id`
-	// be sure to include its associated Product data
+	try {
+		const tagData = await Tag.findByPk(req.params.id, {
+			include: [{ model: Product }],
+		});
+		if (!tagData) {
+			res.status(404).json({
+				message: "No tag found with that id!",
+			});
+			return;
+		}
+		res.status(200).json(tagData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.post("/", (req, res) => {
