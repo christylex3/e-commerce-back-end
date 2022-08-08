1a29663ce126844c38ed289303ce916e34556eb7
Christy Le <christylex3@gmail.com>
2022-08-07 22:45:42 -0400

    Testing error in put request

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -89,11 +89,12 @@ router.put("/:id", (req, res) => {
 						tag_id,
 					};
 				});
+				console.log("newProductTags:" + newProductTags);
 			// figure out which ones to remove
 			const productTagsToRemove = productTags
 				.filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
 				.map(({ id }) => id);
-
+				console.log("productTagsToRemove:" + productTagsToRemove);
 			// run both actions
 			return Promise.all([
 				ProductTag.destroy({ where: { id: productTagsToRemove } }),
@@ -102,7 +103,7 @@ router.put("/:id", (req, res) => {
 		})
 		.then((updatedProductTags) => res.json(updatedProductTags))
 		.catch((err) => {
-			// console.log(err);
+			console.log(err);
 			res.status(400).json(err);
 		});
 });
